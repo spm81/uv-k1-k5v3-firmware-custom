7e84eec2770f81b278e18c5a2f407e4b0fcf5813
superogira / E25WOP <superogira@hotmail.com>
2026-01-22 10:08:18 +0700

    Update Thai Font.

--- App/font.h
+++ App/font.h
@@ -24,6 +24,11 @@ extern const uint8_t gFontBig[95 - 1][16 - 2];
 extern const uint8_t gFontBigDigits[11][26 - 6];
 extern const uint8_t gFont3x5[96][3];
 extern const uint8_t gFontSmall[95 - 1][6];
+
+extern const uint8_t THAI_FONT_BIG[][16];
+extern const uint8_t THAI_FONT_SMALL_Normal[][6];
+extern const uint8_t THAI_FONT_3x5[][3];
+
 #ifdef ENABLE_SMALL_BOLD
     extern const uint8_t gFontSmallBold[95 - 1][6];
 #endif
